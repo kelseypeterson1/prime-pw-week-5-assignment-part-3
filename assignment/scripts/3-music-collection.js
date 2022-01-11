0ee56c175b19c864d9f31fc317f8e51fc9d99997
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-01-11 10:39:05 -0600

    created the addToCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,1 +1,10 @@
 console.log('***** Music Collection *****')
+
+let collection = [];
+function addToCollection(titleInput, artistInput, yearPublishedInput) {
+  let album = {
+    title: titleInput,
+    artist: artistInput,
+    yearPublished : yearPublishedInput
+  }
+}
