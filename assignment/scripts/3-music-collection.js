ec4b0d011ecb6da6084e54ac65da908d2f14c225
Kelsey Peterson <kelsey.peterson14@gmail.com>
2022-01-11 11:03:56 -0600

    Creating and testing the showCollection function

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,8 +2,9 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-// addToCollection function creates an object using the title, artist, and year published input.
-// The object is added to the collection.
+// Creating the addToCollection function.
+// This creates an object using the title, artist, and year published input.
+// The object is then added to the collection.
 function addToCollection(titleInput, artistInput, yearPublishedInput) {
   let album = {
     title: titleInput,
@@ -14,11 +15,22 @@ function addToCollection(titleInput, artistInput, yearPublishedInput) {
   return album;
 }
 
-// Testing the addToCollection function
-console.log("Album added:", addToCollection("Immunity", "Clairo", "08/02/2019"));
-console.log("Album added:", addToCollection("Is This It", "The Strokes", "07/30/2001"));
-console.log("Album added:", addToCollection("Divers", "Joanna Newsom", "10/23/2015"));
-console.log("Album added:", addToCollection("Come Away with Me", "Norah Jones", "02/26/2002"));
-console.log("Album added:", addToCollection("Dear Catastrophe Waitress", "Belle and Sebastian", "10/06/2003"));
-console.log("Album added:", addToCollection("Good at Falling", "The Japanese House", "03/01/2019"));
-console.log("The collection now contains: ",collection);
+// Testing the addToCollection function.
+console.log("Album added:", addToCollection("Immunity", "Clairo", "2019"));
+console.log("Album added:", addToCollection("Is This It", "The Strokes", "2001"));
+console.log("Album added:", addToCollection("Divers", "Joanna Newsom", "2015"));
+console.log("Album added:", addToCollection("Come Away with Me", "Norah Jones", "2002"));
+console.log("Album added:", addToCollection("Dear Catastrophe Waitress", "Belle and Sebastian", "2003"));
+console.log("Album added:", addToCollection("Good at Falling", "The Japanese House", "2019"));
+console.log("The collection now contains:",collection);
+
+// Creating the function showCollection to show the details of each album within.
+function showCollection(array) {
+  console.log("The number of albums in the collection are:",array.length);
+  for (i=0; i<array.length; i++) {
+    console.log(array[i].title, "by", array[i].artist, "published in", array[i].yearPublished);
+  }
+}
+
+// Testing hte showCollection function.
+showCollection(collection);
